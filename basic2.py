a0913b68d0b768125dbdbe8e6902d1b3c5a2a057
kushagra <raikrrish05@gmail.com>
2025-12-29 23:56:13 +0530

    escape sequence

--- basic2.py
+++ basic2.py
@@ -1,4 +1,13 @@
+
+#type conversion
 print(type(str(100)))
 print(type(int("100")))
 print(type(float("3.14")))
 print(type(bool(1)))
+
+#escape sequences
+print("Hello\nWorld")
+print("Hello\tWorld")
+print("He said, \"Hello World!\"")
+print('It\'s a beautiful day!')
+print("C:\\Users\\Name")
