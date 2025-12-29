fa42fd6660df9f61f6f6095526122f83b30e7e95
KUSHAGRA RAI <71624994+05krrish@users.noreply.github.com>
2025-12-29 14:54:32 +0530

    Update passwordcheck.py

--- passwordcheck.py
+++ passwordcheck.py
@@ -1,8 +1,10 @@
 print("GIVE YOUR LOGIN CREDENTIALS :")
 username=input("USERNAME : ")
-password: str=input("PASSWORD : ")
+password: str=input("PASSWORD : ")#how?
 
 if username=='admin' and password=='1234':
     print("LOGIN SUCCESSFUL")
 else:
     print("LOGIN FAILED")
+    
+    
