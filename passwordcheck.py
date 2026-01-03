5f092a6d885566b85c46f9d1076fe0116ec088ce
kushagra <raikrrish05@gmail.com>
2026-01-03 13:38:43 +0530

    show hide func password check!!

--- passwordcheck.py
+++ passwordcheck.py
@@ -1,10 +1,34 @@
-print("GIVE YOUR LOGIN CREDENTIALS :")
-username=input("USERNAME : ")
-password: str=input("PASSWORD : ")#how?
+import tkinter as tk
 
-if username=='admin' and password=='1234':
-    print("LOGIN SUCCESSFUL")
-else:
-    print("LOGIN FAILED")
-    
-    
+root = tk.Tk()
+root.title("Password Checker")
+root.geometry("300x200")
+
+def toggle_password():
+    if password_entry.cget("show") == "":
+        password_entry.config(show="*")
+    else:
+        password_entry.config(show="")
+
+password_entry = tk.Entry(root, show="*")
+password_entry.pack(pady=10)
+
+toggle_btn = tk.Button(root, text="Show / Hide", command=toggle_password)
+toggle_btn.pack()
+
+root.mainloop()
+def toggle_password():
+    if show_var.get():
+        password_entry.config(show="")
+    else:
+        password_entry.config(show="*")
+
+show_var = tk.IntVar()
+
+password_entry = tk.Entry(root, show="*")
+password_entry.pack()
+
+show_check = tk.Checkbutton(root, text="Show Password",
+                            variable=show_var,
+                            command=toggle_password)
+show_check.pack()
